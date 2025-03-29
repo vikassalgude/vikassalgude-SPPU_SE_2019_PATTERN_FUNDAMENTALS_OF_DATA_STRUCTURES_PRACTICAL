dababdc7967ee9b0e6982ee8ce24d2b7e865cb5f
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-03-30 00:48:48 +0530

    Update DAY123.cpp

--- DAY123.cpp
+++ DAY123.cpp
@@ -39,3 +39,24 @@ class Solution {
         return cost;
     }
 };
+// LAST STONE WEIGHT
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& arr) {
+        priority_queue<int>q;
+        for(int i=0;i<arr.size();i++){
+            q.push(arr[i]);
+        }
+        //int cost=0;
+        while(q.size()>1){
+            int a=q.top();q.pop();
+            int b=q.top();q.pop();
+            //cost+=a-b;
+            q.push(a-b);
+         
+        }
+        
+        return q.top();
+    }
+};
+
